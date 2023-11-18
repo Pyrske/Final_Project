cac273ce3a322cca33d6f7e201665d6866951d07
LGugs3 <148275393+LGugs3@users.noreply.github.com>
2023-11-18 13:44:37 -0500

    Update header.h
    updated 11/18/23

--- header.h
+++ header.h
@@ -1,11 +1,28 @@
-#ifndef HEADER_H
-#define HEADER_H
+#pragma once
 
-//put includes here
-//maybe make namespace
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cmath>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <fstream>
 
-//functions titles here
-//put constants here
+using namespace std;
 
 
-#endif
+void makeShop();
+void removeShop();
+bool addMoney(const string& input);
+string findLocation(const string& input);
+void playerAction(const string& input);
+bool changeLocation(const string& input);
+int getShopLength();
+void openShop();
+void buyItem();
+void removeShopItem(const string& itemName);
+unsigned int difficultyLine(const string& difficulty);
+void dungeon();
+void recordEnemies();
+void getEnemy(const string &difficulty);
