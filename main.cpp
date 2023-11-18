6502d79d6980c4350a56eedd8c8c309f2e21892d
LGugs3 <148275393+LGugs3@users.noreply.github.com>
2023-11-18 13:45:05 -0500

    Update main.cpp
    updated 11/18/23

--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
 #include "header.h"
 
-//main file here
+int main() {
+    string userInput;
+    makeShop();
+    recordEnemies();
+    while (getline(cin, userInput)){
+        if (userInput=="exit") {//if player wants to exit the game
+            removeShop();
+            break;
+        }
+        else{
+            playerAction(userInput);
+        }
+    }
+}
